6531508b04652a8f63d9cf3ad15566adada194f9
Keith Devens <keith@keithdevens.com>
2022-07-11 01:17:35 -0600

    First working version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,44 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { readdirSync } from "fs";
+import { join } from "path";
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "binrun" is now active!');
+const DEFAULT_SUBFOLDERS = ["bin"];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('binrun.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from binrun!');
-	});
+function getTasks() {
+  var tasks: string[][] = [];
+  vscode.workspace.workspaceFolders?.forEach((folder) => {
+    const subfolders = DEFAULT_SUBFOLDERS; // todo: make config option
 
-	context.subscriptions.push(disposable);
+    subfolders.forEach((sub) => {
+      const dir = join(folder.uri.fsPath, sub);
+      const files = readdirSync(dir);
+      files.forEach((path) => tasks.push([sub, path]));
+    });
+  });
+  return tasks;
 }
 
-// this method is called when your extension is deactivated
+function makeOpt(subdir: string, file: string) {
+  const path = join(subdir, file);
+  return { label: file, description: `execute '${path}'`, command: path };
+}
+
+function showOptions() {
+  var options = getTasks().map((task) => makeOpt(task[0], task[1]));
+  vscode.window.showQuickPick(options).then((option) => {
+    if (!option || !option.command || option.command.length === 0) {
+      return;
+    }
+    var term =
+      vscode.window.activeTerminal || vscode.window.createTerminal("binrun");
+    term.sendText(option.command);
+  });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("binrun.show", showOptions)
+  );
+}
+
 export function deactivate() {}
